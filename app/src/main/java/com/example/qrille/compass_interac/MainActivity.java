6d8fdd6a9aad1a8990a9c75d53dfa7745567375f
Qrille <aux_sound@hotmail.com>
2015-03-25 18:41:20 +0100

    hello

--- app/src/main/java/com/example/qrille/compass_interac/MainActivity.java
+++ app/src/main/java/com/example/qrille/compass_interac/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //This is comment
         setContentView(R.layout.activity_main);
     }
 
